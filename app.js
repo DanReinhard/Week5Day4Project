8735f7b202beccdd9186025be2a16175b92f7b81
DanReinhard <home.daniel@gmail.com>
2021-05-11 18:42:43 -0600

    file for recreation

---
+++ app.js
@@ -0,0 +1,5 @@
+let change1 = $(".navItem").css('color', 'white');
+let change2 = $("#nav").css('background-color', '#145');
+let change3 = $("li:even").css('background-color', '#ccc');
+let change4 = $("li:odd").css('background-color', '#aaa');
+let change5 = $("input").val('Daniel');
